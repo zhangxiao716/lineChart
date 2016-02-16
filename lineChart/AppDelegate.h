d860dda4e58defe9268539b2fd7708522cac4480
zhangxiao4223 <cc@ccdeimac.local>
2016-02-16 09:00:18 +0800

    Initial Commit

---
+++ lineChart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lineChart
+//
+//  Created by cc on 16/2/16.
+//  Copyright © 2016年 zx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lineChart/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  lineChart
+//
+//  Created by cc on 16/2/16.
+//  Copyright © 2016年 zx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
